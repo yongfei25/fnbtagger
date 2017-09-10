b26fc5fd42c48079d7a9e3d0e6a60ba6632aa67f
Yong Fei <yongfei@outlook.my>
2017-09-10 11:57:24 +0800

    Add GetIds function.

--- fnbtagger/generate_examples.py
+++ fnbtagger/generate_examples.py
@@ -1,4 +1,5 @@
 import sys
+# import tensorflow as tf
 
 
 def extract_tokens(sentence):
@@ -9,11 +10,29 @@ def extract_labels(sentence):
     return [x.split('/').pop().upper() for x in sentence.split(' ')]
 
 
+# def make_example(sequence, labels):
+#     # The object we return
+#     ex = tf.train.SequenceExample()
+#     # A non-sequential feature of our example
+#     sequence_length = len(sequence)
+#     ex.context.feature["length"].int64_list.value.append(sequence_length)
+#     # Feature lists for the two sequential features of our example
+#     fl_tokens = ex.feature_lists.feature_list["tokens"]
+#     fl_labels = ex.feature_lists.feature_list["labels"]
+#     for token, label in zip(sequence, labels):
+#         fl_tokens.feature.add().int64_list.value.append(token)
+#         fl_labels.feature.add().int64_list.value.append(label)
+#     return ex
+
+
 class TokenIndexer:
-    def __init__(self):
-        self.last_id = 0
+    def __init__(self, unk='unk'):
+        self.last_id = 1
         self.ids = {}
         self.tokens = {}
+        self.ids[unk] = self.last_id
+        self.tokens[self.last_id] = unk
+        self.unk = unk
 
     def index(self, sentence, extract_func):
         tokens = extract_func(sentence)
@@ -28,7 +47,14 @@ class TokenIndexer:
                 indexes.append(self.last_id)
         return indexes
 
+    def get_ids(self, tokens):
+        return [self.ids.get(token, self.ids[self.unk]) for token in tokens]
 
+    def get_tokens(self, ids):
+        return [self.tokens.get(tId, self.tokens[1]) for tId in ids]
+
+
+# TODO: Split train/dev/test datasets
 def main(_):
     data_path = './data/annotations.txt'
     with open(data_path) as file:
--- fnbtagger/tests/test.py
+++ fnbtagger/tests/test.py
@@ -9,6 +9,8 @@ class GenerateExampleTestCase(TestCase):
         super(GenerateExampleTestCase, self).__init__(*args, **kwargs)
         self.sentence = ('The/O quick/B-ORG brown/I-ORG fox/I-ORG jumps/O '
                          'over/O the/O lazy/B-PER dog/I-PER ./O')
+        self.sentence2 = ('Sometimes life is going to hit you in the head '
+                          'with a brick .')
 
     def test_extract_tokens(self):
         tokens = extract_tokens(self.sentence)
@@ -25,11 +27,24 @@ class GenerateExampleTestCase(TestCase):
     def test_token_indexer(self):
         indexer = TokenIndexer()
         result = indexer.index(self.sentence, extract_tokens)
-        expected = [1, 2, 3, 4, 5, 6, 1, 7, 8, 9]
+        expected = [2, 3, 4, 5, 6, 7, 2, 8, 9, 10]
         self.assertEqual(result, expected)
+        self.assertEqual(len(list(indexer.tokens.keys())), 10)
+        self.assertEqual(len(list(indexer.ids.keys())), 10)
 
     def test_labels_indexer(self):
-        indexer = TokenIndexer()
+        indexer = TokenIndexer(unk='O')
         result = indexer.index(self.sentence, extract_labels)
         expected = [1, 2, 3, 3, 1, 1, 1, 4, 5, 1]
         self.assertEqual(result, expected)
+        self.assertEqual(len(list(indexer.tokens.keys())), 5)
+        self.assertEqual(len(list(indexer.ids.keys())), 5)
+
+    def test_tokens_get_ids(self):
+        indexer = TokenIndexer()
+        indexer.index(self.sentence, extract_tokens)
+        tokens = extract_tokens(self.sentence2)
+        ids = indexer.get_ids(tokens)
+        # the 1's are UNK (unknown token)
+        expected = [1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 10]
+        self.assertEqual(ids, expected)
